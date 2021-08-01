0919e5b9a9a30fab66dd907425951987f2e723f0
kevvrites <liukproductivity@gmail.com>
2021-08-01 13:38:49 -0400

    Created template file for future problems

---
+++ leetcode_template.py
@@ -0,0 +1,32 @@
+# Author: Kevin Liu
+# Problems available at https://leetcode.com/
+# Start Date: 00/00/0000
+# Start Time: 00:00 PM ET
+# Complete Date: 00/00/0000
+# Complete Time: 00:00 PM ET
+# Note: Problem may or may not be completed in one sitting.
+
+# Problem Name
+# Problem URL https://leetcode.com/problems//
+
+# Problem Description
+
+# Example Cases
+
+# Constraints
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+# Thought process:
+# 1. 
+# 2. 
+# 3. 
+# 4. 
+
+# Test Cases
+
+def funcName():
+    return
+
+# Submission Result:
+# Runtime:      0000 ms (top 00.00%)
+# Memory Usage: 00.0 MB (top 00.00%)
