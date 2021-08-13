124f3ece7e8f07edf0d7615cad2d6d2aaf7b9ccd
Kevvrites <liukproductivity@gmail.com>
2021-08-13 15:57:20 -0400

    Set up MergeTwoSortedLists file format

---
+++ Merge_Two_Sorted_Lists.py
@@ -0,0 +1,45 @@
+# Author: Kevin Liu
+# Problems available at https://leetcode.com/
+# Start Date: 00/00/0000
+# Start Time: 00:00 PM ET
+# Complete Date: 00/00/0000
+# Complete Time: 00:00 PM ET
+# Note: Problem may or may not be completed in one sitting.
+
+# Merge Two Sorted Lists
+# https://leetcode.com/problems/merge-two-sorted-lists/
+
+# Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Example Cases
+# Example 1
+# Input: l1 = [1,2,4], l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+
+# Example 2:
+# Input: l1 = [], l2 = []
+# Output: []
+
+# Example 3:
+# Input: l1 = [], l2 = [0]
+# Output: [0]
+
+# Constraints
+# The number of nodes in both lists is in the range [0, 50].
+# -100 <= Node.val <= 100
+# Both l1 and l2 are sorted in non-decreasing order.
+# ------------------------------------------------------------------------------------------------------------------------------------
+# Thought process:
+# 1. 
+# 2. 
+# 3. 
+# 4. 
+
+# Test Cases
+
+def funcName():
+    return
+
+# Submission Result:
+# Runtime:      0000 ms (top 00.00%)
+# Memory Usage: 00.0 MB (top 00.00%)
