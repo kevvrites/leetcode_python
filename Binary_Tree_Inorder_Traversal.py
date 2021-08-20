a26f0328551fc2508b5652af51b9b1bc957c2eb3
Kevvrites <liukproductivity@gmail.com>
2021-08-20 15:21:51 -0400

    Binary Tree Problem On Hold

---
+++ Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,54 @@
+# Author: Kevin Liu
+# Problems available at https://leetcode.com/
+# Start Date: 00/00/0000
+# Start Time: 00:00 PM ET
+# Complete Date: 00/00/0000
+# Complete Time: 00:00 PM ET
+# Note: Problem may or may not be completed in one sitting.
+
+# Binary Tree Inorder Traversal
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+# Example Cases
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+
+# Example 2:
+# Input: root = []
+# Output: []
+
+# Example 3:
+# Input: root = [1]
+# Output: [1]
+
+# Example 4:
+# Input: root = [1,2]
+# Output: [2,1]
+
+# Example 5:
+# Input: root = [1,null,2]
+# Output: [1,2]
+
+# Constraints
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+# ------------------------------------------------------------------------------------------------------------------------------------
+# Thought process:
+# 1. 
+# 2. 
+# 3. 
+# 4. 
+
+# Test Cases
+
+def funcName():
+    return
+
+# Submission Result:
+# Runtime:      0000 ms (top 00.00%)
+# Memory Usage: 00.0 MB (top 00.00%)
+
+# Follow up: Recursive solution is trivial, could you do it iteratively?
---
+++ Same_Tree.py
@@ -0,0 +1,46 @@
+# Author: Kevin Liu
+# Problems available at https://leetcode.com/
+# Start Date: 00/00/0000
+# Start Time: 00:00 PM ET
+# Complete Date: 00/00/0000
+# Complete Time: 00:00 PM ET
+# Note: Problem may or may not be completed in one sitting.
+
+# Same Tree
+# https://leetcode.com/problems/same-tree/
+
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+# Example Cases
+# Example 1:
+# Input: p = [1,2,3], q = [1,2,3]
+# Output: true
+
+# Example 2:
+# Input: p = [1,2], q = [1,null,2]
+# Output: false
+
+# Example 3:
+# Input: p = [1,2,1], q = [1,1,2]
+# Output: false
+
+# Constraints
+# The number of nodes in both trees is in the range [0, 100].
+# -104 <= Node.val <= 104
+# ------------------------------------------------------------------------------------------------------------------------------------
+# Thought process:
+# 1. 
+# 2. 
+# 3. 
+# 4. 
+
+# Test Cases
+
+def funcName():
+    return
+
+# Submission Result:
+# Runtime:      0000 ms (top 00.00%)
+# Memory Usage: 00.0 MB (top 00.00%)
